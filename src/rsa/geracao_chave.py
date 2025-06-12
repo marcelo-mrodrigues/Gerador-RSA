3c83e4ff863729448b9c6029b9054200406ff7c0
Marcelo Rodrigues <marques.marcelo.rodrigues@gmail.com>
2025-06-12 17:17:53 -0300

    feat: Gerador de Primo

--- src/rsa/geracao_chave.py
+++ src/rsa/geracao_chave.py
@@ -1,3 +1,26 @@
 import random
 from teste_primalidade import talvez_primo
 from sympy import mod_inverse 
+
+def gerar_primo(bits):
+      # Primo aleatório com a quantidade de bits especificada.
+    while True:
+        p = random.getrandbits(bits)
+
+          # Garante exatamente 'bits' de comprimento,
+          # MSB como 1.
+        p |= (1 << bits - 1)
+
+          # -> ímpar
+        p |= 1
+
+        if talvez_primo(p, k=40):
+            return p
+
+
+print(gerar_primo(512))
+print(gerar_primo(1024))
+print(gerar_primo(3))
+print(gerar_primo(5))
+print(gerar_primo(8))
+print(gerar_primo(10))
