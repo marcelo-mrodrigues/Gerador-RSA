da4c24a05ad0459513e2d38671af03558c1b18f2
Marcelo Rodrigues <marques.marcelo.rodrigues@gmail.com>
2025-06-12 17:38:20 -0300

    feat: Gera Chave Pública e Privada

--- src/rsa/geracao_chave.py
+++ src/rsa/geracao_chave.py
@@ -17,10 +17,30 @@ def gerar_primo(bits):
         if talvez_primo(p, k=40):
             return p
 
+def chave_rsa(bits=1024):
+    p = gerar_primo(bits)
+    q = gerar_primo(bits)
+    while p == q:
+        q = gerar_primo(bits)
 
+    n = p * q
+    phi = (p - 1) * (q - 1)
+    e = 65537
+    d = mod_inverse(e, phi)
+    chave_publica = (n, e)
+    chave_privada = (n, d)
+
+    return (chave_publica, chave_privada)
+
+
+
+print(chave_rsa(bits=1024))
+
+
+"""
 print(gerar_primo(512))
 print(gerar_primo(1024))
 print(gerar_primo(3))
 print(gerar_primo(5))
 print(gerar_primo(8))
-print(gerar_primo(10))
+print(gerar_primo(10))"""
